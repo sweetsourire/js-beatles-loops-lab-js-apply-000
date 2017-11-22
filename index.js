d0fdaac778eefaeb837f770a0be039f6608c07e8
Bomi <bkim1289@gmail.com>
2017-11-22 16:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 theBeatlesPlay(musicians, instruments){
-  var newString = [];
+  const newString = [];
   for(let i = 0, i<musicians.length; i+=1){
       arr.push(`${musicians[i]}` plays `${instruments[i]}`)
   }
